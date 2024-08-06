80972ef35ec6a6a96a29f1a4f0f03e2e2743b9e3
Anamika Garg <anamikagarg2906@gmail.com>
2024-08-06 18:38:46 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {connect} = require('mongoose');
 require('dotenv').config();
 const app = express();
 const upload = require("express-fileupload");
-
+const mongoose = require('mongoose')
 const userRoutes = require('./routes/userRoutes')
 const postRoutes = require('./routes/postRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
@@ -27,6 +27,6 @@ app.use(errorHandler);
 
 
 
-connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 5000 , () =>{
+mongoose.connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 5000 , () =>{
     console.log(`Server started on port ${process.env.PORT}`)})).catch(error => console.log(error))
 
