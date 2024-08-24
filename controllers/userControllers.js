c5294277894309435cb35bdeffeaf5caf064fc68
Anamika Garg <anamikagarg2906@gmail.com>
2024-08-24 11:30:18 +0530

    changes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -155,7 +155,7 @@ const changeAvatar = async(req , res, next) =>{
         //delete old avatar , if exists!
         if(user.avatar){
 
-            fs.unlink(path.join(__dirname , '..' , '/uploads' , user.avatar) , (err)=>{
+            fs.unlink(path.join(__dirname , '..' , 'uploads' , user.avatar) , (err)=>{
                 if(err){
                     return next(new HttpError(err))
                 }
@@ -175,7 +175,7 @@ const changeAvatar = async(req , res, next) =>{
         let splittedFileName = fileName.split('.');
         let newFileName = splittedFileName[0] + uuid() + '.' + splittedFileName[splittedFileName.length - 1];
 
-        avatar.mv(path.join(__dirname , '..' , '/uploads' , newFileName) , async(err)=>{
+        avatar.mv(path.join(__dirname , '..' , 'uploads' , newFileName) , async(err)=>{
             if(err){
                 return next(new HttpError(err))
             }
